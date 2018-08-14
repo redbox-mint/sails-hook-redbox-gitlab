56313112dfd3b7cc0b943a7a560eb84818f51b7f
Moises Sacal <moises.sacal@uts.edu.au>
2018-08-14 17:40:58 +1000

    updated config required for UTS config

--- config/workspacetype.js
+++ config/workspacetype.js
@@ -2,8 +2,8 @@ module.exports.workspacetype = {
   'gitlab': {
     name: 'gitlab',
     label: 'GitLab',
-    subtitle: 'The gitlab workspace app',
-    description: 'To Create or link your GitLab to this Plan follow this button (add more text here)',
+    subtitle: 'GitLab',
+    description: 'Create or link GitLab workspace:',
     logo: '/angular/gitlab/assets/images/gitlab.png'
   }
 };
